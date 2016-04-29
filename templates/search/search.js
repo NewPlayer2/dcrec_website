360012a7d378c3f6c0bb2f2567de2745c9b72c25
NewPlayer2 <you.know@what.and.where>
2016-04-29 21:48:18 +0300

    search improvements

--- templates/search/search.js
+++ templates/search/search.js
@@ -1,20 +1,51 @@
 (function () {
+
     $(document).ready(function () {
+
+        $('.collapse').each(function () {
+            $(this).collapse({toggle: $(this).hasClass("collapse-initially-open")});
+        });
+
+        var newindex = 0;
+
         $('.add-player').on('click', function (evt) {
             var proto,
-                clone;
+                clone,
+                oldindex, input;
 
-            proto = $($('.player-condition')[0]);
+            proto = $('.player-condition.original');
             clone = proto.clone();
 
-            clone.removeClass('original');
-            clone.insertBefore($(this));
-            clone.find('input[type=text]').val('').focus();
+            input = clone.find('input[type=text]');
+            oldindex = parseInt(input.attr('name').match(/player\[(\d+)\]/)[1]);
+            newindex = newindex ? newindex + 1 : oldindex + 1;
+            $.each(clone.find(".form-control[name]"), function () {
+                var a = "[" + oldindex + "]",
+                    b = "[" + newindex + "]";
+                $(this).attr("name", $(this).attr("name").replace(a, b));
+            });
+            clone.find('.player-name > .row').not(":first").remove();
+            clone.removeClass('original').insertBefore($(this));
+            input.val('').focus();
             evt.stopPropagation();
             evt.preventDefault();
         });
 
-        $(document).on('click', '.remove-condition', function () {
+        $('form').on('click', '.add-alias', function () {
+            var d = $(this).closest('.row.original'),
+                c = d.clone().removeClass('original');
+            c.find('input').val("");
+            c.insertAfter(d);
+        });
+
+        $('form').on('click', '.remove-alias', function () {
+            var p = $(this).closest('.row');
+            if (!p.hasClass('original')) {
+                p.remove();
+            }
+        });
+
+        $('form').on('click', '.remove-condition', function () {
             var condition = $(this).closest('.player-condition');
             condition.hasClass('original') || condition.remove();
         });
--- templates/search/search.php
+++ templates/search/search.php
@@ -6,54 +6,108 @@
 <script src="<?=$app->site_url('search/script')?>"></script>
 
 <div class="container">
+
   <div class="row">
-  <div class="col-md-6">
-    <form class="form-horizontal" role="form" method="post" action="<?=htmlspecialchars($app->site_url('search/results/1'))?>">
-        <div class="form-group">
-        <label for="filter">Server</label>
-        <select class="form-control" name="server">
-        <option value="0" selected>Any server</option>
-        <?php foreach ($servers as $serverid => $server): ?>
-        <option value="<?=(int)$serverid?>"><?=htmlspecialchars($server)?></option>
-        <?php endforeach; ?>
-        </select>
-        </div>
-        <?php if (!isset($players) || empty($players)) { $players = array(""); } ?>
-        <?php foreach ($players as $idx => $playerpost) : ?>
-            <div class="form-group player-condition <?=($idx ? "" : "original")?>">
+  <div class="col-md-8">
+    <h1>Search</h1>
+    <form class="form-horizontal" role="form" method="post" action="<?=$backend?>">
+        <?php $players = (!isset($_POST['player']) || empty($_POST['player']) ? array(0 => "") : $_POST['player']); ?>
+        <?php foreach ($players as $playerindex => $aliases) : ?>
+            <?php
+            $playerindex = (int) $playerindex;
+            $played = (!isset($_POST['player'][$playerindex]) || $_POST['played'][$playerindex]);
+            ?>
+
+            <div class="form-group player-condition <?=($playerindex ? "" : "original")?>">
                 <label>
-                    <span>Player </span>
+                    <span>Player set </span>
                     <button type="button" class="btn btn-xs btn-default remove-condition">
                         <span class="glyphicon glyphicon-remove text-danger" aria-hidden="true"></span>Remove
                     </button>
                 </label>
                 <div class="row">
-                <div class="col-xs-3 connective-col">
-                    <select name="connective[]" class="form-control">
-                        <option value="OR" selected>OR</option>
-                        <option value="AND" class="disabled" disabled>AND</option>
-                    </select>
-                </div>
-                <div class="col-xs-9">
-                    <input name="player[]" class="form-control" type="text" value="<?=htmlspecialchars($playerpost)?>"/>
-                </div>
+                  <div class="col-xs-3">
+                      <select name="played[<?=$playerindex?>]" class="form-control played-status">
+                          <option value="1" <?=($played  ? "selected" : "")?>>Played</option>
+                          <option value="0" <?=(!$played ? "selected" : "")?>>Did not play</option>
+                      </select>
+                  </div>
+                  <div class="col-xs-9 player-name">
+                  <?php if (!is_array($aliases)) { $aliases = array(""); } ?>
+
+                  <?php foreach ($aliases as $aliasindex => $alias): ?>
+                      <div class="row<?=($aliasindex === 0 ? " original" : "")?>">
+                          <div class="col-xs-9">
+                          <input name="player[<?=$playerindex?>][]" class="form-control" type="text" value="<?=htmlspecialchars($alias)?>"/>
+                          </div>
+                          <div class="col-xs-3">
+                          <button class="btn btn-xs btn-default add-alias" type="button"><span class="glyphicon glyphicon-plus"></span>alt form (OR)</button>
+                          <button class="btn btn-xs btn-default remove-alias" type="button"><span class="glyphicon glyphicon-remove"></span>Remove</button>
+                          </div>
+                      </div>
+                  <?php endforeach; ?>
+                  </div>
                 </div>
             </div>
         <?php endforeach; ?>
-        <div class="help-block">
-        <a class="add-player btn btn-default btn-xs" href="javascript:void(0);">Add a player</a>
-        </div>
-        <button type="submit" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-search" aria-hidden="true"></span>Search</button>
-    </form>
-    </div>
-    <div class="col-md-5 col-md-push-1">
-    <h3>How to search</h3>
-    <p>By default, the player name search phrase is matched as is. However, the % character can be used as a wildcard.</p>
-    <p>For example the search for <mark class="kw">Ne%yer</mark> would match games with <span class="kw">NewPlayer</span> as well as <span class="kw">New Freaking Player</span> or <span class="kw">Nerdy annoyer</span>.</p>
-    <p>If you need to search for player names containing the wildcard character, wrap your keywords with double quotes ("). For example the search phrase <mark class="kw">100%efficiency</mark> <strong>would</strong> match a player named <span class="kw">100%<strong>in</strong>efficiency</span> but <mark class="kw">"100%efficiency"</mark> <strong>would not</strong>.</p>
-    <p>Use the white button to add multiple player names to your search. Unfortunately using <span>AND</span> as a connective is currently disabled.</p>
-    </div>
 
+        <div class="form-group">
+        <p><button class="add-player btn btn-default btn-xs" type="button"><span class="glyphicon glyphicon-plus"></span>Add player set</button></p>
+        </div>
+
+
+
+        <?php
+        if (isset($_POST['servers'])) { $_POST['servers'] = array_filter($_POST['servers']); }
+        if (isset($_POST['notservers'])) { $_POST['notservers'] = array_filter($_POST['notservers']); }
+        $serverfilters = (isset($_POST['servers']) && !empty($_POST['servers']));
+        $serverexcludes = (isset($_POST['notservers']) && !empty($_POST['notservers']));
+        ?>
+
+        <div class="form-group">
+        <label data-toggle="collapse" data-target="#serverfilter-collapsible"><a href="javascript:void(0);"><span class="glyphicon glyphicon-collapse-down"></span>Filter by server</a></label>
+        <div class="collapse <?=($serverfilters ? "collapse-initially-open" : "")?>" id="serverfilter-collapsible">
+        <p class="help-block">You can select multiple servers. Select the first option or leave unselected to search games played on any servers.</p>
+        <select id="server-filter" class="form-control" name="servers[]" multiple>
+        <option value="0" <?=(!$serverfilters ? "selected" : "")?>>-- Any server --</option>
+        <?php foreach ($servers as $serverid => $server): ?>
+            <option value="<?=(int)$serverid?>" <?=(isset($_POST['servers']) && in_array($serverid, $_POST['servers']) ? "selected" : "")?>>
+            <?=htmlspecialchars($server)?>
+            </option>
+        <?php endforeach; ?>
+        </select>
+        </div>
+        </div>
+
+        <div class="form-group">
+        <label data-toggle="collapse" data-target="#exclude-collapsible"><a href="javascript:void(0);"><span class="glyphicon glyphicon-collapse-down"></span>Exclude servers</a></label>
+        <div class="collapse <?=($serverexcludes ? "collapse-initially-open" : "")?>" id="exclude-collapsible">
+        <p class="help-block">Select server(s) to exclude from your search.</p>
+        <select id="exclude-server-filter" class="form-control" name="notservers[]" multiple>
+        <option value="0" <?=(!$serverexcludes ? "selected" : "")?>>-- No exclusions --</option>
+        <?php foreach ($servers as $serverid => $server): ?>
+            <option value="<?=(int)$serverid?>" <?=(isset($_POST['notservers']) && in_array($serverid, $_POST['notservers']) ? "selected" : "")?>>
+            <?=htmlspecialchars($server)?>
+            </option>
+        <?php endforeach; ?>
+        </select>
+        </div>
+        </div>
+
+        <div>
+        <button type="submit" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-search" aria-hidden="true"></span>Search</button>
+        </div>
+
+    </form>
+  </div>
+  <div class="col-md-4" style="border-left: 1px solid #eee;">
+    <h2>How to search?</h2>
+    <p>By default, the player name search phrase is matched as is. However, the <mark>%</mark> character can be used as a wildcard.</p>
+    <p>The search for <mark class="kw">Ne%yer</mark> would match games with <span class="kw">NewPlayer</span> as well as <span class="kw">New Freaking Player</span> or <span class="kw">Nerdy annoyer</span>.</p>
+    <p>If you need to search for player names containing the wildcard character itself, wrap your keywords with double quotes (<mark>"</mark>). For example: the search phrase <mark class="kw">100%efficiency</mark> <strong>would</strong> match a player named <span class="kw">100%<strong>in</strong>efficiency</span> but <mark class="kw">"100%efficiency"</mark> <strong>would not</strong>.</p>
     </div>
+  </div>
+
 </div>
+
 <hr />
